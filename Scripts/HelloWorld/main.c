cef46106508be17e71e771fd57aca1c65eca20cd
Happykelee <jety2858@163.com>
2019-05-31 17:37:21 +0800

    update-2

--- Scripts/HelloWorld/main.c
+++ Scripts/HelloWorld/main.c
@@ -4,5 +4,11 @@
 int main()
 {
     printf("Hello world!\n");
+
+
+      for (int number = 0;number != 27;scanf("%d", &number)){
+        printf("Please input 27:");
+      }
+      printf("Done!");
     return 0;
 }
